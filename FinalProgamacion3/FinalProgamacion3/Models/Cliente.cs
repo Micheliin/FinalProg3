e83f251291a650be5b7918c038a20cf409ea8384
Micheliin Arias ♥ <mishyarias0410@gmail.com>
2020-04-08 11:12:42 -0400

    CRUD Clientes

---
+++ FinalProgamacion3/FinalProgamacion3/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FinalProgamacion3.Models
+{
+    public class Cliente
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int IDClientes { get; set; }
+        [Required]
+        public int RNC { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Telefono { get; set; }
+        [RegularExpression(@"\w+([-+.']\w+)@\w+([-.]\w+)\.\w+([-.]\w+)*",
+            ErrorMessage = "Dirección de Correo electrónico incorrecta.")]
+        public string Email { get; set; }
+        public int IDCategoria { get; set; }
+
+    }
+}
---
+++ FinalProgamacion3/FinalProgamacion3/Models/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+
+namespace FinalProgamacion3.Models
+{
+    public class Context:DbContext
+        
+    {
+        public Context()
+           : base("name-Context")
+
+        {
+        }
+        public virtual DbSet<Cliente> Clientes { get; set; }
+        
+    }
+}
